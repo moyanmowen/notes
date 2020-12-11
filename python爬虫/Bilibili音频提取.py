3cd5a4ccbf69abceb995b54b04790babd82b76ed
moyanmowen <69954232+moyanmowen@users.noreply.github.com>
2020-12-11 19:57:26 +0800

    Add files via upload

---
+++ python爬虫/Bilibili音频提取.py
@@ -0,0 +1,101 @@
+import requests
+from lxml import etree
+import json
+import os
+import sys
+import asyncio
+import aiohttp
+from time import time
+
+
+async def getData(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
+
+async def schedule(url, datalist):
+    for html in audio_link(await getData(url)):
+        datalist.append(html)
+
+
+def audio_link(data):
+    # 定位到script
+    html = etree.HTML(data)  # 创建element对象
+    scripts = html.xpath("/html/head/script[5]/text()")[0]
+    script = scripts.split("window.__playinfo__=")[1]
+    script_json = json.loads(script)
+    # 获取标题
+    title = html.xpath('//*[@id="viewbox_report"]/h1/@title')[0]
+    error_set = ['/', r'\\', ':', '*', '?', '"', '|', '<', '>']
+    # 去除特殊符号
+    for i in title:
+        if i in error_set:
+            title = title.replace(i, '')
+    # 解析音频的地址
+    audio_url = script_json["data"]['dash']["audio"][0]["baseUrl"]
+    yield {"audio_url": audio_url, "title": title}
+
+
+def download(audio_url, title, headers):
+    if os.path.exists(location+"%s.mp3" % title):
+        print("文件已存在")
+        return
+    if "logo=80000000" in audio_url:
+        audiocontent = session.get(audio_url, headers=headers).content
+    else:
+        audiocontent = requests.get(audio_url).content
+
+    with open(location + '%s.mp3' % title, 'wb') as f:
+        f.write(audiocontent)
+        f.close()
+
+
+def main():
+    try:
+        datalist = []
+        threads = []
+        url = input("输入链接(多链接时，以英文逗号隔开):")
+        if "www.bilibili.com" not in url:
+            print("输入正确的网址")
+            return
+        link = url.split(',')
+        loop = asyncio.get_event_loop()
+        for url in link:
+            threads.append(schedule(url, datalist))
+        loop.run_until_complete(asyncio.wait(threads))
+        for i, bili in enumerate(datalist):
+            print("任务：{z}/{x}".format(z=i+1, x=len(datalist)))
+            download(bili["audio_url"], bili["title"], headers)
+            print("{}下载完成".format(bili["title"]))
+        num = input("输入0退出,回车继续>")
+        if num == "0":
+            loop.stop()
+            loop.close()
+            sys.exit()
+    except requests.exceptions.MissingSchema:
+        print("出现bug，系统错误")
+        sys.exit()
+
+
+if __name__ == '__main__':
+
+    print("-----Bililili音频提取-----")
+    location = "./Bilibili_audio/"
+    session = requests.session()
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "identity",
+        "accept-language": "zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6,zh-HK;q=0.5",
+        "origin": "https://www.bilibili.com",
+        "range": "bytes=0-169123900000000",
+        "referer": "https://www.bilibili.com/video/",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.41"
+    }
+    if not os.path.exists(location):
+        os.mkdir(location)
+    while True:
+        main()
